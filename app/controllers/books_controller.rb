d4d36a9515cf435885645d339f27831369e2d68e
EC2 Default User <ec2-user@ip-172-31-12-222.ec2.internal>
2022-03-06 10:13:03 +0000

    [Add]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else
-     render :new
+     @books = Book.all
+     render :index
     end
   end
 
@@ -24,10 +25,13 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+     redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+    else
+     render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
